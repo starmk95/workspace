ed15b6ea690c595fe129230a8d05efc34b18b08e
sk25a054 <sk25a054>
2025-02-06 10:50:49 +0900

    20250206 데이터분석 수업 시작

---
+++ Data_Analysis/Practice.py
@@ -0,0 +1,18 @@
+def ex1_1():
+    a = 20
+    b = 30
+    c = a+b
+    print(c)
+
+def ex1_2():
+    x=20
+    Computer = 'Mac' 
+    Age = 30
+    My_score = 70
+    _name = '김민규'
+    myBirthYear = 1997
+    data2 = 20.3
+    print(x, Computer, My_score, _name, myBirthYear, data2)
+
+# ex1_1
+ex1_2()
